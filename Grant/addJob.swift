76665b6da4bfb8701844bbf1820c9179f01d25e5
sasakazu <sasakura.company@gmail.com>
2018-10-24 19:35:19 +0900

    file add done....

---
+++ Grant/addJob.swift
@@ -0,0 +1,27 @@
+//
+//  addJob.swift
+//  Grant
+//
+//  Created by 笹倉 一也 on 2018/10/24.
+//  Copyright © 2018年 sasakura.company. All rights reserved.
+//
+
+import UIKit
+
+class addJob: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addJobDone(_ sender: Any) {
+    }
+    
+    @IBAction func cancelBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+}
---
+++ Grant/working.swift
@@ -0,0 +1,21 @@
+//
+//  working.swift
+//  Grant
+//
+//  Created by 笹倉 一也 on 2018/10/24.
+//  Copyright © 2018年 sasakura.company. All rights reserved.
+//
+
+import UIKit
+
+class working: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
