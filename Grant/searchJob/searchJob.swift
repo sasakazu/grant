96a6e4e07cb0dcbd1fd50ce230426e9e4e862b4e
笹倉一也 <>
2018-11-24 17:30:46 +0900

    segment control done...

--- Grant/searchJob/searchJob.swift
+++ Grant/searchJob/searchJob.swift
@@ -7,11 +7,20 @@
 //
 
 import UIKit
+import RealmSwift
 
-class searchJob: UIViewController {
-
+class searchJob: UIViewController
+//,UITableViewDelegate, UITableViewDataSource
+{
+ 
+    @IBOutlet weak var tableView1: UITableView!
+    
+    @IBOutlet weak var tableView2: UITableView!
+    
     @IBOutlet weak var segmentControl: UISegmentedControl!
     
+    @IBOutlet weak var tableView3: UITableView!
+    @IBOutlet var segmentView0: UIView!
     @IBOutlet var segmentView1: UIView!
     @IBOutlet var segmentView2: UIView!
     
@@ -20,8 +29,17 @@ class searchJob: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+//        addZeroView()
+        self.view.addSubview(segmentView0)
         
     }
+    
+    func addZeroView() {
+        segmentView1.removeFromSuperview()
+        self.view.addSubview(segmentView0)
+        
+    }
+    
     
     func addFirstView() {
         segmentView2.removeFromSuperview()
@@ -30,7 +48,7 @@ class searchJob: UIViewController {
     }
     
     func addSecondView() {
-        segmentView1.removeFromSuperview()
+        segmentView0.removeFromSuperview()
         self.view.addSubview(segmentView2)
         
     }
@@ -40,16 +58,29 @@ class searchJob: UIViewController {
         switch sender.selectedSegmentIndex {
         
         case 0:
-        addFirstView()
+            addZeroView()
         case 1:
-        addSecondView()
+            addFirstView()
+        case 2:
+            addSecondView()
         default:
-        addFirstView()
+            addZeroView()
         
         }
         
     }
+
+
     
-
-
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+    
+    
+    
+    
 }
---
+++ Grant/searchJob/searchNewJob.swift
@@ -0,0 +1,21 @@
+//
+//  searchNewJob.swift
+//  Grant
+//
+//  Created by 笹倉一也 on 2018/11/24.
+//  Copyright © 2018 sasakura.company. All rights reserved.
+//
+
+import UIKit
+
+class searchNewJob: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
