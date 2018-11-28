f5d50f7a13e4c3d4cd841d635f550ad0a68076f2
笹倉一也 <>
2018-11-28 19:20:01 +0900

    icon add ......

--- Grant/searchJob/searchJob.swift
+++ Grant/searchJob/searchJob.swift
@@ -10,75 +10,82 @@ import UIKit
 import RealmSwift
 
 class searchJob: UIViewController
-//,UITableViewDelegate, UITableViewDataSource
+,UITableViewDelegate, UITableViewDataSource
 {
- 
+    
+    let searchCategory = [
+        "身の回り","細かな作業","お掃除","わんちゃんのこと","体力仕事","外作"
+    ]
+    
+    
+    let searchPlace = [
+        
+        "北海道","青森県","岩手県","宮城県","秋県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"
+        
+    ]
+    
+    
+    var jobOfferItem: Results<JobOffer>!
+    
+    
+    
     @IBOutlet weak var tableView1: UITableView!
     
-    @IBOutlet weak var tableView2: UITableView!
-    
     @IBOutlet weak var segmentControl: UISegmentedControl!
-    
-    @IBOutlet weak var tableView3: UITableView!
-    @IBOutlet var segmentView0: UIView!
-    @IBOutlet var segmentView1: UIView!
-    @IBOutlet var segmentView2: UIView!
+ 
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        addZeroView()
-//        self.view.addSubview(segmentView0)
         
+        do{
+            let realm = try Realm()
+            
+            jobOfferItem = realm.objects(JobOffer.self)
+            
+            
+        }catch{
+            
+            
+        }
+        
+        
+        tableView1.dataSource = self
+        tableView1.delegate = self
+    
     }
     
-//    func addZeroView() {
-//        segmentView1.removeFromSuperview()
-//        self.view.addSubview(segmentView0)
-//
-//    }
-//
-//
-//    func addFirstView() {
-//        segmentView2.removeFromSuperview()
-//        self.view.addSubview(segmentView1)
-//
-//    }
-//
-//    func addSecondView() {
-//        segmentView0.removeFromSuperview()
-//        self.view.addSubview(segmentView2)
-//
-//    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    
+        tableView1.reloadData()
+    }
+    
     
     @IBAction func segmentTapped(_ sender: UISegmentedControl) {
         
-//        switch sender.selectedSegmentIndex {
         
-//        case 0:
-////            addZeroView()
-//        case 1:
-////            addFirstView()
-//        case 2:
-////            addSecondView()
-//        default:
-////            addZeroView()
-        
-//        }
         
     }
 
 
     
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        <#code#>
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        <#code#>
-//    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return jobOfferItem.count
+    
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        
+        let object = jobOfferItem[indexPath.row]
+        
+        cell.textLabel?.text = object.title
+        
+        return cell
+    }
     
     
     
